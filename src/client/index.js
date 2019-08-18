cca385ca3491ce7281ad6d1dabb7bf53c60d79f5
deciduously <ben@deciduously.com>
2019-08-18 16:25:33 -0400

    Add commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,3 @@
 // Nothing here yet!  It will eventually subscribe to the wasm memory
+// The entrypoint is marked #[wasm_bindgen(start)], so it's called automatically when instantiated in the import
 import * as wasm from "fivedice";
