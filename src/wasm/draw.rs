b2b7fcc650c9daceeca9d031e08f6a4af2cb51e1
deciduously <ben@deciduously.com>
2019-08-21 21:18:47 -0400

    More refactor

--- src/wasm/draw.rs
+++ src/wasm/draw.rs
@@ -1,10 +1,18 @@
 // draw.rs contains the Drawable/Clickable traits and canvas rendering engine, as well as generic widgets like Button
 use crate::{
     error::*,
-    ffi::{get_canvas, get_context},
+    ffi::{get_body, get_canvas, get_context, get_document, request_animation_frame},
 };
-use std::{cell::Cell, cmp::Ordering, fmt, ops::AddAssign, rc::Rc, str::FromStr};
-use wasm_bindgen::JsValue;
+use std::{
+    cell::{Cell, RefCell},
+    cmp::Ordering,
+    fmt,
+    ops::AddAssign,
+    rc::Rc,
+    str::FromStr,
+};
+use wasm_bindgen::{prelude::*, JsCast, JsValue};
+use web_sys::{CanvasRenderingContext2d, MouseEvent};
 
 // You somehow need each thing to know where it is
 // You need a better abstraction over the canvas.
@@ -496,6 +504,8 @@ lazy_static! {
 /// Window error type
 #[derive(Debug)]
 pub enum WindowError {
+    Element,
+    JsVal(JsValue),
     OutOfBounds,
     Text,
 }
@@ -503,9 +513,23 @@ pub enum WindowError {
 impl fmt::Display for WindowError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Self::Element => write!(f, "Could not append element to DOM"),
+            Self::JsVal(js) => write!(f, "{:#?}", js),
             Self::Text => write!(f, "Could not add text to the window"),
             Self::OutOfBounds => write!(f, "Attempted to scroll cursor out of bounds!"),
         }
+    }
+}
+
+impl From<JsValue> for WindowError {
+    fn from(other: JsValue) -> Self {
+        WindowError::JsVal(other)
+    }
+}
+
+impl Into<JsValue> for WindowError {
+    fn into(self) -> JsValue {
+        format!("{}", self).into()
     }
 }
 
@@ -533,6 +557,73 @@ pub trait Window {
 
 /// Alias for a reference-counted pointer to a Window object
 pub type WindowPtr = Rc<Box<dyn Window>>;
+
+/// Canvas implementation for WebSys
+pub struct WebSysCanvas {
+    ctx: Rc<CanvasRenderingContext2d>,
+}
+
+impl WebSysCanvas {
+    pub fn new(title: &str) -> WindowResult<Self> {
+        // Set up page
+        let document = get_document();
+        let body = get_body();
+        // Mount the title and canvas elements
+        append_text_element_attrs!(document, body, "h1", title,);
+        append_element_attrs!(
+            document,
+            body,
+            "canvas",
+            ("width", &format!("{}", VALUES.canvas_size.0)),
+            ("height", &format!("{}", VALUES.canvas_size.1))
+        );
+
+        Ok(Self::default())
+    }
+}
+
+impl Default for WebSysCanvas {
+    fn default() -> Self {
+        Self {
+            ctx: Rc::new(get_context()),
+        }
+    }
+}
+
+impl Window for WebSysCanvas {
+    fn blank(&self) {
+        self.ctx.clear_rect(
+            0.0,
+            0.0,
+            VALUES.canvas_size.0.into(),
+            VALUES.canvas_size.1.into(),
+        )
+    }
+    fn rect(&self, region: Region) {
+        self.ctx.rect(
+            region.origin().x,
+            region.origin().y,
+            region.width(),
+            region.height(),
+        );
+    }
+    fn begin_path(&self) {
+        self.ctx.begin_path();
+    }
+    fn draw_path(&self) {
+        self.ctx.stroke();
+    }
+    fn set_color(&self, color: Color) {
+        self.ctx.set_stroke_style(&format!("{}", color).into());
+    }
+    fn text(&self, text: &str, font: &str, origin: Point) -> WindowResult<()> {
+        self.ctx.set_font(font);
+        if self.ctx.fill_text(text, origin.x, origin.y).is_err() {
+            return Err(WindowError::Text);
+        }
+        Ok(())
+    }
+}
 
 /// Top-level canvas engine object
 /// // TODO maybe a good spot to store values?
@@ -562,6 +653,51 @@ impl WindowEngine {
         let w = Rc::clone(&self.window);
         self.element.draw(w)?;
         Ok(())
+    }
+
+    /// Start engine
+    pub fn start(self) {
+        let engine = Rc::new(RefCell::new(self));
+        // Add click listener
+        // translate from page coords to canvas coords
+        // shamelessly lifted from the RustWasm book but translated to Rust
+        // https://rustwasm.github.io/book/game-of-life/interactivity.html
+        {
+            //let engine = engine.clone();
+            let callback = Closure::wrap(Box::new(move |evt: MouseEvent| {
+                let canvas = get_canvas();
+                let bounding_rect = canvas.get_bounding_client_rect();
+                let scale_x = f64::from(canvas.width()) / bounding_rect.width();
+                let scale_y = f64::from(canvas.height()) / bounding_rect.height();
+
+                let canvas_x = (f64::from(evt.client_x()) - bounding_rect.left()) * scale_x;
+                let canvas_y = (f64::from(evt.client_y()) - bounding_rect.top()) * scale_y;
+
+                //TODO implement Clickable
+                //engine.borrow_mut().handle_click(canvas_x, canvas_y);
+                let _click: Point = (canvas_x, canvas_y).into();
+            }) as Box<dyn FnMut(_)>);
+
+            get_canvas()
+                .add_event_listener_with_callback("click", callback.as_ref().unchecked_ref())
+                .expect("Could not register event listener");
+            callback.forget();
+        }
+        {
+            // Run the game loop
+            // All iterations inside the loop can use the Rc.  Starts out empty
+            let f = Rc::new(RefCell::new(None));
+            let g = f.clone();
+            *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+                engine
+                    .borrow()
+                    .draw()
+                    .unwrap_or_else(|_| panic!("Window error on draw"));
+                request_animation_frame(f.borrow().as_ref().unwrap());
+            }) as Box<dyn FnMut()>));
+            // Kick off the loop
+            request_animation_frame(g.borrow().as_ref().unwrap());
+        }
     }
 }
 
--- src/wasm/ffi.rs
+++ src/wasm/ffi.rs
@@ -1,16 +1,15 @@
 // ffi.rs contains all JS<->Rust interop
 
 use crate::{
-    draw::{Color, Point, Region, Window, WindowEngine, WindowError, WindowResult, VALUES},
+    draw::{WebSysCanvas, WindowEngine},
     game::Game,
 };
 use js_sys::Math::{floor, random};
-use std::{cell::RefCell, rc::Rc};
 use wasm_bindgen::{prelude::*, JsCast};
-use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlElement, MouseEvent};
+use web_sys::{CanvasRenderingContext2d, Document, HtmlCanvasElement, HtmlElement};
 
 /// Grab the body
-fn get_body() -> HtmlElement {
+pub fn get_body() -> HtmlElement {
     get_document().body().expect("No <body> found in document")
 }
 
@@ -35,7 +34,7 @@ pub fn get_context() -> CanvasRenderingContext2d {
 }
 
 /// Grab the document
-fn get_document() -> Document {
+pub fn get_document() -> Document {
     get_window()
         .document()
         .expect("No document found on window")
@@ -47,7 +46,7 @@ fn get_window() -> web_sys::Window {
 }
 
 /// requestAnimationFrame
-fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+pub fn request_animation_frame(f: &Closure<dyn FnMut()>) {
     get_window()
         .request_animation_frame(f.as_ref().unchecked_ref())
         .expect("should register `requestAnimationFrame`");
@@ -58,125 +57,20 @@ pub fn js_gen_range(min: i64, max: i64) -> i64 {
     (floor(random() * (max as f64 - min as f64)) + min as f64) as i64
 }
 
-/// Canvas implementation for WebSys
-pub struct WebSysCanvas {
-    ctx: Rc<CanvasRenderingContext2d>,
-}
-
-impl WebSysCanvas {
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Default for WebSysCanvas {
-    fn default() -> Self {
-        Self {
-            ctx: Rc::new(get_context()),
-        }
-    }
-}
-
-impl Window for WebSysCanvas {
-    fn blank(&self) {
-        self.ctx.clear_rect(
-            0.0,
-            0.0,
-            VALUES.canvas_size.0.into(),
-            VALUES.canvas_size.1.into(),
-        )
-    }
-    fn rect(&self, region: Region) {
-        self.ctx.rect(
-            region.origin().x,
-            region.origin().y,
-            region.width(),
-            region.height(),
-        );
-    }
-    fn begin_path(&self) {
-        self.ctx.begin_path();
-    }
-    fn draw_path(&self) {
-        self.ctx.stroke();
-    }
-    fn set_color(&self, color: Color) {
-        self.ctx.set_stroke_style(&format!("{}", color).into());
-    }
-    fn text(&self, text: &str, font: &str, origin: Point) -> WindowResult<()> {
-        self.ctx.set_font(font);
-        if self.ctx.fill_text(text, origin.x, origin.y).is_err() {
-            return Err(WindowError::Text);
-        }
-        Ok(())
-    }
-}
-
 /// Entry point for the module
 #[allow(dead_code)]
 #[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
-    // Set canvas dimensions
-    let document = get_document();
-    let body = get_body();
-    // Mount the title and canvas elements
-    append_text_element_attrs!(document, body, "h1", "FIVE DICE",);
-    append_element_attrs!(
-        document,
-        body,
-        "canvas",
-        ("width", &format!("{}", VALUES.canvas_size.0)),
-        ("height", &format!("{}", VALUES.canvas_size.1))
-    );
-
-    // Instantiate game engine
-    let renderable_context = Box::new(WebSysCanvas::new());
+pub fn start() {
+    // Instantiate canvas
+    let renderable_context =
+        Box::new(WebSysCanvas::new("Five Dice").expect("Could not instantiate canvas"));
 
     // Instantiate game
     let game = Box::new(Game::new());
 
-    // Instantiate Canvas engine
-    let engine = Rc::new(RefCell::new(WindowEngine::new(renderable_context, game)));
+    // Instantiate engine
+    let engine = WindowEngine::new(renderable_context, game);
 
-    // Add click listener
-    // translate from page coords to canvas coords
-    // shamelessly lifted from the RustWasm book but translated to Rust
-    // https://rustwasm.github.io/book/game-of-life/interactivity.html
-    {
-        //let engine = engine.clone();
-        let callback = Closure::wrap(Box::new(move |evt: MouseEvent| {
-            let canvas = get_canvas();
-            let bounding_rect = canvas.get_bounding_client_rect();
-            let scale_x = f64::from(canvas.width()) / bounding_rect.width();
-            let scale_y = f64::from(canvas.height()) / bounding_rect.height();
-
-            let canvas_x = (f64::from(evt.client_x()) - bounding_rect.left()) * scale_x;
-            let canvas_y = (f64::from(evt.client_y()) - bounding_rect.top()) * scale_y;
-
-            //TODO implement Clickable
-            //engine.borrow_mut().handle_click(canvas_x, canvas_y);
-            let _click: Point = (canvas_x, canvas_y).into();
-        }) as Box<dyn FnMut(_)>);
-
-        get_canvas()
-            .add_event_listener_with_callback("click", callback.as_ref().unchecked_ref())
-            .expect("Could not register event listener");
-        callback.forget();
-
-        // Run the game loop
-        // All iterations inside the loop can use the Rc.  Starts out empty
-        let f = Rc::new(RefCell::new(None));
-        let g = f.clone();
-        *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-            engine
-                .borrow()
-                .draw()
-                .expect(&format!("Window error on draw"));
-            request_animation_frame(f.borrow().as_ref().unwrap());
-        }) as Box<dyn FnMut()>));
-        // Kick off the loop
-        request_animation_frame(g.borrow().as_ref().unwrap());
-
-        Ok(())
-    }
+    // Run game
+    engine.start();
 }
