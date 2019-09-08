ea80f3d3c7b1a68c11f6fcde91b76d8f62f1f312
deciduously <ben@deciduously.com>
2019-09-08 00:09:22 -0400

    Make deploy

--- docs/bootstrap.js
+++ docs/bootstrap.js
@@ -321,7 +321,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/fivedice_bg.wasm":"274453b0ad62b029055a"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"./pkg/fivedice_bg.wasm":"c65d101c8a5592e9a022"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
